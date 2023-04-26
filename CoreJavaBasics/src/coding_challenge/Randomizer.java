b696fcad739cbd6d0d881599b114d3d8cca4fb08
Bokar Dieng <43213652+bocar90@users.noreply.github.com>
2023-04-25 21:13:36 -0400

    Randomizer

--- CoreJavaBasics/src/coding_challenge/Randomizer.java
+++ CoreJavaBasics/src/coding_challenge/Randomizer.java
@@ -1,10 +1,35 @@
 package coding_challenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Randomizer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random rand = new Random(); 
+		 
+		String[] names = {"Harold", "James", "John", "Andrew", "Philip", "Matthew", "Thomas", "Simon"};
+		
+		List<String> cloneNames =  new ArrayList<String>(Arrays.asList(names));;
+		
+		String []randString = new String[names.length];
+		
+		int i = 0;
+		
+		while(!cloneNames.isEmpty()) {
+			int int_random = rand.nextInt(names.length);
+			if(cloneNames.contains(names[int_random])) {
+				randString[i] = names[int_random];
+				cloneNames.remove(names[int_random]);
+				i++;
+			}
+		}
+		
+		System.out.println("Orignal array: " + Arrays.toString(names));
+		System.out.println("Random array:  " + Arrays.toString(randString));
 	}
 
 }
